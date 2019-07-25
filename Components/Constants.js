8e7282785b294d2b1eee2123f3a63b0ce3ec707e
dale2508 <datle2508@gmail.com>
2019-07-25 13:27:01 +0700

    Revert "x"
    This reverts commit 61828e8a0b71cdc5970350c8db5e8e20277faaec.

--- Components/Constants.js
+++ Components/Constants.js
@@ -2,10 +2,8 @@
 * @providesModule Fonts
 */
 const CONSTANTS = {
-    // PS_URL:'http://10.36.126.14:13133/peoplesoft',
-    // LOGIN_URL:'http://10.36.126.14:11131/Authentication',
-    PS_URL:'http://lsowsuat.lienvietpostbank.com.vn/peoplesoft',
-    LOGIN_URL:'http://lsowsuat.lienvietpostbank.com.vn/Authentication',
+    PS_URL:'http://10.36.126.14:13133/peoplesoft',
+    LOGIN_URL:'http://10.36.126.14:11131/Authentication',
     CHECKPERMISION_URL:'OpenSans-SemiBold',
     PAGE_SIZE:10,
     MSG_ID_PS:1231312,
